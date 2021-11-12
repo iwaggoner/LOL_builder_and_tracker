e84a10e2a6906e292efa91eeafe10e1ab2947aec
iwaggoner <iwaggoner95@gmail.com>
2021-11-12 17:44:58 -0500

    Create a home profile page with a link that sends to champion page with all of champs that links to page with details on the champions.

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ const session = require('express-session')
 const passport = require('./config/ppConfig')
 const flash = require('connect-flash')
 const isLoggedIn = require('./middleware/isLoggedIn')
+const fs = require('fs')
+const champData = fs.readFileSync('11.22.1/data/en_US/champion.json')
+const champs = JSON.parse(champData)
+const itemData = fs.readFileSync('11.22.1/data/en_US/item.json')
+const items = JSON.parse(itemData)
 
 
 // views (ejs and layouts) set up
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
+
+app.use(express.static('public'));
 
 // body parser middelware
 app.use(express.urlencoded({extended:false}))
@@ -43,12 +50,25 @@ app.use('/auth', require('./controllers/auth'))
 
 // home route
 app.get('/', (req, res)=>{
-    res.render('home')
+    res.render('home', {})
 })
 
 // profile route
 app.get('/profile', isLoggedIn, (req, res)=>{
+    
     res.render('profile')
+})
+
+app.get('/champs', isLoggedIn, (req, res)=> {
+    const arrayOfChamps = Object.getOwnPropertyNames(champs.data)
+
+    res.render('champs/champ', {champsObj : champs, champsNames: arrayOfChamps})
+})
+
+app.get('/champs/:name', isLoggedIn, (req, res)=>{
+    const name = req.params.name
+    res.render('champs/names', {champsObj : champs, name: name})
+    console.log(champs.data[name])
 })
 
 
